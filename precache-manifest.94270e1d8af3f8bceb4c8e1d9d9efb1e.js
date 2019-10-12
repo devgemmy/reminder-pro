fc0ab4194841a5b64c5dda33da5f60c8610d45ee
DevGemmy <anthonia@nugitech.com>
2019-10-12 13:29:20 +0100

    Updates

---
+++ precache-manifest.94270e1d8af3f8bceb4c8e1d9d9efb1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c4153ce1e41d6e48d3009898e77549",
+    "url": "/reminder-pro/index.html"
+  },
+  {
+    "revision": "7bc82a58ca5b868fea17",
+    "url": "/reminder-pro/static/css/main.0e336d50.chunk.css"
+  },
+  {
+    "revision": "e6baabb9a2e1689fff1c",
+    "url": "/reminder-pro/static/js/2.68acd6f9.chunk.js"
+  },
+  {
+    "revision": "7bc82a58ca5b868fea17",
+    "url": "/reminder-pro/static/js/main.080104b2.chunk.js"
+  },
+  {
+    "revision": "27b6fe9958108c40f1ef",
+    "url": "/reminder-pro/static/js/runtime~main.809c8ecd.js"
+  }
+]);
